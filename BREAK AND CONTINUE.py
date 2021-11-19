a6e1641a0b5ea66db75c0b191c52ba285c9e7e01
Sangeet <Sangeetkukreja26@gmail.com>
2021-11-20 01:37:59 +0530

    Initial Commit

---
+++ BREAK AND CONTINUE.py
@@ -0,0 +1,37 @@
+#ef funj(A,C):
+#   print(C)
+  # for item in A:
+#       print(item)
+# 'Sam ttttb ttb '
+#= ["harry","shyam","ram","vindo"]
+#unj(s,D)
+#
+ # def Ram(Cut):
+#     for item in Cut:
+#         print(item)
+#
+
+
+# name= ["Sameer","Vindo"]
+#
+# Ram(name)
+
+# def funtion(Sam , Normal):
+#     for item in Sam,Normal:
+#          print(item)
+# Sham=[ "Vinod","Sak"]
+# Har = ["Sam","Ram"]
+#
+#
+#
+# funtion(Sham,Har)
+#
+# def funtion(Sam,  Dis  ):
+#     for item in Sam:
+#         print(item)
+#   for key,Value in Dis.items():
+#        print(Value)
+# `   print(Dis)
+# dict    ={"Ram": " Program", "Shan":  "Enginner"}
+# Har=    ["Sangeet","Shan"]
+# funtion(Har,dict)
